68fd3ff852f8b60c1766b9f38d9fa6e79eb8c60a
YUNG <shaioking@gmail.com>
2021-01-21 13:52:26 +0800

    encoding_utf_8

--- products1.py
+++ products1.py
@@ -13,3 +13,9 @@ while True:
 print(products)
 for p in products:
 	print(p[0], '的價格是', p[1], '元')
+
+with open('products.csv', 'w') as f:#encoding='utf-8'
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '元' + '\n')
+#txt 改成 csv
